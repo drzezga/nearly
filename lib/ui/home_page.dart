7bbaf347634ada3faeb0ebc1bc907e7248f9e77c
drzezga <19654689+drzezga@users.noreply.github.com>
2022-10-15 17:11:25 +0200

    Refactored

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hackathon/settings_page.dart';
+import 'settings_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
