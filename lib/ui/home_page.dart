2d419ee8cc998d762452b690768699b3fff857b5
jakubluc <107751484+jakubluc@users.noreply.github.com>
2022-10-16 04:59:48 +0200

    Payload

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -11,16 +11,17 @@ import '../controller/requirement_state_controller.dart';
 import 'package:get/get.dart';
 import 'dart:async';
 
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
 
   final String title;
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   StreamSubscription<RangingResult>? _streamRanging;
   final _regionBeacons = <Region, List<Beacon>>{};
   final _beacons = <Beacon>[];
@@ -28,85 +29,84 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-  super.initState();
+    super.initState();
 
-  controller.startStream.listen((flag) {
-  if (flag == true) {
-  initScanBeacon();
-  }
-  });
+    controller.startStream.listen((flag) {
+      if (flag == true) {
+        initScanBeacon();
+      }
+    });
 
-  controller.pauseStream.listen((flag) {
-  if (flag == true) {
-  pauseScanBeacon();
-  }
-  });
+    controller.pauseStream.listen((flag) {
+      if (flag == true) {
+        pauseScanBeacon();
+      }
+    });
   }
 
   initScanBeacon() async {
-  await flutterBeacon.initializeScanning;
-  if (!controller.authorizationStatusOk ||
-  !controller.locationServiceEnabled ||
-  !controller.bluetoothEnabled) {
-  print(
-  'RETURNED, authorizationStatusOk=${controller.authorizationStatusOk}, '
-  'locationServiceEnabled=${controller.locationServiceEnabled}, '
-  'bluetoothEnabled=${controller.bluetoothEnabled}');
-  return;
-  }
-  final regions = <Region>[
-  Region(
-  identifier: 'Cubeacon',
-  proximityUUID: 'CB10023F-A318-3394-4199-A8730C7C1AEC',
-  ),
-  Region(
-  identifier: 'BeaconType2',
-  proximityUUID: '6a84c716-0f2a-1ce9-f210-6a63bd873dd9',
-  ),
-  ];
+    await flutterBeacon.initializeScanning;
+    if (!controller.authorizationStatusOk ||
+        !controller.locationServiceEnabled ||
+        !controller.bluetoothEnabled) {
+      print(
+          'RETURNED, authorizationStatusOk=${controller.authorizationStatusOk}, '
+          'locationServiceEnabled=${controller.locationServiceEnabled}, '
+          'bluetoothEnabled=${controller.bluetoothEnabled}');
+      return;
+    }
+    final regions = <Region>[
+      Region(
+        identifier: 'Cubeacon',
+        proximityUUID: 'CB10023F-A318-3394-4199-A8730C7C1AEC',
+      ),
+      Region(
+        identifier: 'BeaconType2',
+        proximityUUID: '6a84c716-0f2a-1ce9-f210-6a63bd873dd9',
+      ),
+    ];
 
-  if (_streamRanging != null) {
-  if (_streamRanging!.isPaused) {
-  _streamRanging?.resume();
-  return;
-  }
-  }
+    if (_streamRanging != null) {
+      if (_streamRanging!.isPaused) {
+        _streamRanging?.resume();
+        return;
+      }
+    }
 
-  _streamRanging =
-  flutterBeacon.ranging(regions).listen((RangingResult result) {
-  print(result);
-  if (mounted) {
-
-  setState(() {
-  _regionBeacons[result.region] = result.beacons;
-  _beacons.clear();
-  _regionBeacons.values.forEach((list) {
-  _beacons.addAll(list);
-  });
-  _beacons.sort(_compareParameters);
-  });
-  }
-  });
+    _streamRanging =
+        flutterBeacon.ranging(regions).listen((RangingResult result) {
+      print(result);
+      if (mounted) {
+        setState(() {
+          _regionBeacons[result.region] = result.beacons;
+          _beacons.clear();
+          _regionBeacons.values.forEach((list) {
+            _beacons.addAll(list);
+          });
+          _beacons.sort(_compareParameters);
+        });
+      }
+    });
   }
 
   pauseScanBeacon() async {
-  _streamRanging?.pause();
-  if (_beacons.isNotEmpty) {
-  setState(() {
-  _beacons.clear();
-  });
-  }
+    _streamRanging?.pause();
+    if (_beacons.isNotEmpty) {
+      setState(() {
+        _beacons.clear();
+      });
+    }
   }
 
   int _compareParameters(Beacon a, Beacon b) {
-  int compare = a.proximityUUID.compareTo(b.proximityUUID);
+    int compare = a.proximityUUID.compareTo(b.proximityUUID);
 
     if (compare == 0) {
-    compare = a.major.compareTo(b.major);
+      compare = a.major.compareTo(b.major);
     }
 
     if (compare == 0) {
-    compare = a.minor.compareTo(b.minor);
+      compare = a.minor.compareTo(b.minor);
     }
 
     return compare;
@@ -114,33 +114,29 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void dispose() {
-  _streamRanging?.cancel();
-  super.dispose();
+    _streamRanging?.cancel();
+    super.dispose();
   }
 
-
-
-
-    @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar.large(
               title: const Text("Nearly",
-                  style: TextStyle(
-                      fontSize: 40, fontWeight: FontWeight.w800)),
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.w800)),
               titleSpacing: 0,
               actions: [
                 IconButton(
                     icon: const Icon(Icons.settings),
                     onPressed: () => {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            //     builder: (context) => BlocProvider(create: (_) => NotificationPreferencesCubit(), child: const SettingsPage())),
-                              builder: (_) => const SettingsPage()))
-                    })
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  //     builder: (context) => BlocProvider(create: (_) => NotificationPreferencesCubit(), child: const SettingsPage())),
+                                  builder: (_) => const SettingsPage()))
+                        })
               ]),
           SliverToBoxAdapter(child: BlocBuilder<TagBloc, List<Tag>>(
             builder: (context, state) {
@@ -150,7 +146,9 @@ class _HomePageState extends State<HomePage> {
                     Padding(
                         padding: const EdgeInsets.only(
                             left: 10.0, right: 10.0, top: 10.0),
-                        child: ReadTagCard(tag, onTap: () {_showTagSheet(context, tag);}))
+                        child: ReadTagCard(tag, onTap: () {
+                          _showTagSheet(context, tag);
+                        }))
                 ],
               );
             },
@@ -175,9 +173,8 @@ class _HomePageState extends State<HomePage> {
         expand: false,
         builder: (context, scrollController) => SafeArea(
           child: TagDetailsPage(tag),
-          ),
         ),
-      );
-
+      ),
+    );
   }
 }
