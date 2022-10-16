205f781120035f763e16ce4e08edfe9ba02e4e3f
jakubluc <107751484+jakubluc@users.noreply.github.com>
2022-10-16 05:13:29 +0200

    payload2 electric boogaloo

--- lib/model/payload.dart
+++ lib/model/payload.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hackathon/model/tag.dart';
 
 class Payload {
   final IconData icon;
   final String displayName;
   final String description;
 
-  const Payload(this.displayName, this.icon, this.description);
+  const Payload(this.displayName, this.icon, this.description, this.type);
+
+  final TagType type;
 }
