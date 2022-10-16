039e2a9983d84aadcf7ceeba451413e82028b0ea
jakubluc <107751484+jakubluc@users.noreply.github.com>
2022-10-16 05:16:29 +0200

    Update payload.dart

--- lib/model/payload.dart
+++ lib/model/payload.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hackathon/model/tag.dart';
 
 class Payload {
-  final IconData icon;
-  final String displayName;
   final String description;
 
   const Payload(this.displayName, this.icon, this.description, this.type);
