286fb8c485fcd9c116156d31cabc659c82f4cc0f
jakubluc <107751484+jakubluc@users.noreply.github.com>
2022-10-16 02:28:25 +0200

    Changes realted to android dev

--- lib/ui/settings_page.dart
+++ lib/ui/settings_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hackathon/bloc/settings_cubit.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'dev_page.dart';
+
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -16,7 +18,13 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Ustawienia')),
+        appBar: AppBar(title: const Text('Ustawienia'), actions: [IconButton(onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  //     builder: (context) => BlocProvider(create: (_) => NotificationPreferencesCubit(), child: const SettingsPage())),
+                    builder: (_) => const DevPage()));
+        }, icon: Icon(Icons.abc))],),
         body: BlocBuilder<NotificationPreferencesCubit, Map<String, bool>>(builder: (context, state) {
           return SettingsList(
             sections: [
@@ -40,4 +48,5 @@ class _SettingsPageState extends State<SettingsPage> {
           );
         }));
   }
+
 }
